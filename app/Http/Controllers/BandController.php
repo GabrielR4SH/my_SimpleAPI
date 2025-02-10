e6c8bab31858aca0106dcd4e313f2543c3436c4f
GabrielR4SH <bieelsantana23@hotmail.com>
2025-02-09 23:09:14 -0300

    feat: store method in BandsController

--- app/Http/Controllers/BandController.php
+++ app/Http/Controllers/BandController.php
@@ -15,8 +15,8 @@ class BandController extends Controller
     public function getById($id)
     {
         $band = null;
-        foreach($this->getBands() as $_band){
-            if($_band['id'] == $id){
+        foreach ($this->getBands() as $_band) {
+            if ($_band['id'] == $id) {
                 $band = $_band;
             }
         }
@@ -33,15 +33,43 @@ class BandController extends Controller
         ];
     }
 
-    public function getBandsByGender($gender){
+    public function getBandsByGender($gender)
+    {
         $bands = [];
 
-        foreach($this->getBands() as $_band){
-            if($_band['gender'] == $gender){
+        foreach ($this->getBands() as $_band) {
+            if ($_band['gender'] == $gender) {
                 $bands[] = $_band;
             }
         }
     }
 
-    
+    public function store(Request $request)
+    {
+        // Validação dos dados recebidos
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'gender' => 'required|string|max:255',
+        ]);
+
+        // Obter a lista atual de bandas
+        $bands = $this->getBands();
+
+        // Gerar um novo ID para a banda
+        $newId = count($bands) + 1;
+
+        // Criar a nova banda
+        $newBand = [
+            'id' => $newId,
+            'name' => $request->input('name'),
+            'gender' => $request->input('gender'),
+        ];
+
+        // Adicionar a nova banda à lista
+        $bands[] = $newBand;
+
+        // Retornar a nova banda como resposta JSON
+        return response()->json($newBand, 201);
+    }
+
 }
